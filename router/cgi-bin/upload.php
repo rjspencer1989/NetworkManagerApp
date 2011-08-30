98035d84d7812203cc90df9ce486c4842cf59208
Robert Spencer <signup@robspencer.me.uk>
2011-08-30 22:10:29 +0100

    Licensed Router and VM under GPL

--- router/cgi-bin/upload.php
+++ router/cgi-bin/upload.php
@@ -1,3 +1,17 @@
+//Copyright 2011 Robert Spencer
+//This file is part of NetworkManagerApp Router
+//NetworkManagerApp Router is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//NetworkManagerApp Router is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with NetworkManagerApp Router.  If not, see <http://www.gnu.org/licenses/>.
 #!/usr/bin/php-cgi
 <?php
 date_default_timezone_set("Europe/London");
--- vm/update-rrd.sh
+++ vm/update-rrd.sh
@@ -1,30 +1,45 @@
+#Copyright 2011 Robert Spencer
+#This file is part of NetworkManagerApp Router
+#NetworkManagerApp Router is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#NetworkManagerApp Router is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with NetworkManagerApp Router.  If not, see <http://www.gnu.org/licenses/>. 
+
 #!/bin/bash
 cd /var/www/html/weathermap
-echo "#Weathermap configuration File" > /var/www/html/weathermap/weathermap-2.conf
-echo "HEIGHT 600" >> /var/www/html/weathermap/weathermap-2.conf
-echo "WIDTH 740" >> /var/www/html/weathermap/weathermap-2.conf
-echo "FONT 4" >> /var/www/html/weathermap/weathermap-2.conf
-echo "LEGENDPOS 300 460" >> /var/www/html/weathermap/weathermap-2.conf
-echo "LEGENDSTYLE line" >> /var/www/html/weathermap/weathermap-2.conf
-echo "TITLE \"Home Network Map\"" >> /var/www/html/weathermap/weathermap-2.conf
-echo "TITLEPOS 4 470" >> /var/www/html/weathermap/weathermap-2.conf
-echo "TITLEBACKGROUND 255 255 128" >> /var/www/html/weathermap/weathermap-2.conf
-echo "TITLEFOREGROUND 0 0 0" >> /var/www/html/weathermap/weathermap-2.conf
-echo "REFRESH 60" >> /var/www/html/weathermap/weathermap-2.conf
-echo "OUTPUTFILE /var/www/html/weathermap/weathermap.png" >> /var/www/html/weathermap/weathermap-2.conf
-echo "HTMLFILE /var/www/html/weathermap/weathermap.html" >> /var/www/html/weathermap/weathermap-2.conf
-echo "SCALE  1  10 140   0  255" >> /var/www/html/weathermap/weathermap-2.conf
-echo "SCALE 10  25  32  32  255" >> /var/www/html/weathermap/weathermap-2.conf
-echo "SCALE 25  40   0 192  255" >> /var/www/html/weathermap/weathermap-2.conf
-echo "SCALE 40  55   0 240    0" >> /var/www/html/weathermap/weathermap-2.conf
-echo "SCALE 55  70 240 240    0" >> /var/www/html/weathermap/weathermap-2.conf
-echo "SCALE 70  85 255 192    0" >> /var/www/html/weathermap/weathermap-2.conf
-echo "SCALE 85  90 255 100    0" >> /var/www/html/weathermap/weathermap-2.conf
-echo "SCALE 90 100 255   0    0" >> /var/www/html/weathermap/weathermap-2.conf 
-echo "NODE router" >> /var/www/html/weathermap/weathermap-2.conf
-echo "POSITION 60 40" >> /var/www/html/weathermap/weathermap-2.conf
-echo "LABEL Router" >> /var/www/html/weathermap/weathermap-2.conf
-echo "ICON icons/wireless_router.png" >> /var/www/html/weathermap/weathermap-2.conf
+echo "#Weathermap configuration File" > /var/www/html/weathermap/weathermap.conf
+echo "HEIGHT 600" >> /var/www/html/weathermap/weathermap.conf
+echo "WIDTH 740" >> /var/www/html/weathermap/weathermap.conf
+echo "FONT 4" >> /var/www/html/weathermap/weathermap.conf
+echo "LEGENDPOS 300 460" >> /var/www/html/weathermap/weathermap.conf
+echo "LEGENDSTYLE line" >> /var/www/html/weathermap/weathermap.conf
+echo "TITLE \"Home Network Map\"" >> /var/www/html/weathermap/weathermap.conf
+echo "TITLEPOS 4 470" >> /var/www/html/weathermap/weathermap.conf
+echo "TITLEBACKGROUND 255 255 128" >> /var/www/html/weathermap/weathermap.conf
+echo "TITLEFOREGROUND 0 0 0" >> /var/www/html/weathermap/weathermap.conf
+echo "REFRESH 60" >> /var/www/html/weathermap/weathermap.conf
+echo "OUTPUTFILE /var/www/html/weathermap/weathermap.png" >> /var/www/html/weathermap/weathermap.conf
+echo "HTMLFILE /var/www/html/weathermap/weathermap.html" >> /var/www/html/weathermap/weathermap.conf
+echo "SCALE  1  10 140   0  255" >> /var/www/html/weathermap/weathermap.conf
+echo "SCALE 10  25  32  32  255" >> /var/www/html/weathermap/weathermap.conf
+echo "SCALE 25  40   0 192  255" >> /var/www/html/weathermap/weathermap.conf
+echo "SCALE 40  55   0 240    0" >> /var/www/html/weathermap/weathermap.conf
+echo "SCALE 55  70 240 240    0" >> /var/www/html/weathermap/weathermap.conf
+echo "SCALE 70  85 255 192    0" >> /var/www/html/weathermap/weathermap.conf
+echo "SCALE 85  90 255 100    0" >> /var/www/html/weathermap/weathermap.conf
+echo "SCALE 90 100 255   0    0" >> /var/www/html/weathermap/weathermap.conf 
+echo "NODE router" >> /var/www/html/weathermap/weathermap.conf
+echo "POSITION 60 40" >> /var/www/html/weathermap/weathermap.conf
+echo "LABEL Router" >> /var/www/html/weathermap/weathermap.conf
+echo "ICON icons/wireless_router.png" >> /var/www/html/weathermap/weathermap.conf
 for f in *.txt 
 do
 name=${f%.*}".rrd"
@@ -40,18 +55,18 @@ then
 rrdtool create $name --start N --step=60 DS:out:GAUGE:60:U:U DS:in:GAUGE:60:U:U RRA:AVERAGE:0.5:1:24
 fi
 rrdtool update $name N:$bytesout:$bytesin
-echo "NODE "${f%.*} >> /var/www/html/weathermap/weathermap-2.conf
-echo "POSITION " $x $y >> /var/www/html/weathermap/weathermap-2.conf
-echo "LABEL "$labeltext >> /var/www/html/weathermap/weathermap-2.conf
-echo "LINK" ${f%.*}"-router" >> /var/www/html/weathermap/weathermap-2.conf
-echo "NODES router ${f%.*}" >> /var/www/html/weathermap/weathermap-2.conf
-echo "TARGET /var/www/html/weathermap/"$name >> /var/www/html/weathermap/weathermap-2.conf
-echo "INPOS 1" >> /var/www/html/weathermap/weathermap-2.conf
-echo "OUTPOS 2" >> /var/www/html/weathermap/weathermap-2.conf
-echo "UNIT bytes" >> /var/www/html/weathermap/weathermap-2.conf
-echo "BANDWIDTH 50000" >> /var/www/html/weathermap/weathermap-2.conf
-echo "DISPLAYVALUE 1" >> /var/www/html/weathermap/weathermap-2.conf
-echo "ARROW normal" >> /var/www/html/weathermap/weathermap-2.conf
+echo "NODE "${f%.*} >> /var/www/html/weathermap/weathermap.conf
+echo "POSITION " $x $y >> /var/www/html/weathermap/weathermap.conf
+echo "LABEL "$labeltext >> /var/www/html/weathermap/weathermap.conf
+echo "LINK" ${f%.*}"-router" >> /var/www/html/weathermap/weathermap.conf
+echo "NODES router ${f%.*}" >> /var/www/html/weathermap/weathermap.conf
+echo "TARGET /var/www/html/weathermap/"$name >> /var/www/html/weathermap/weathermap.conf
+echo "INPOS 1" >> /var/www/html/weathermap/weathermap.conf
+echo "OUTPOS 2" >> /var/www/html/weathermap/weathermap.conf
+echo "UNIT bytes" >> /var/www/html/weathermap/weathermap.conf
+echo "BANDWIDTH 50000" >> /var/www/html/weathermap/weathermap.conf
+echo "DISPLAYVALUE 1" >> /var/www/html/weathermap/weathermap.conf
+echo "ARROW normal" >> /var/www/html/weathermap/weathermap.conf
 
 done
 rm -f /var/www/html/weathermap/*.txt

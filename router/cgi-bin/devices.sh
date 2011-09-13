cb4f96056addea39f84a8ed70742148558f130dc
Robert Spencer <signup@robspencer.me.uk>
2011-09-13 21:06:41 +0100

    device script now outputs to JSON instead of XML. Needs testing on router to ensure json is valid

--- router/cgi-bin/devices.sh
+++ router/cgi-bin/devices.sh
@@ -13,8 +13,9 @@
 #You should have received a copy of the GNU General Public License
 #along with NetworkManagerApp Router.  If not, see <http://www.gnu.org/licenses/>. 
 #!/bin/sh
-path=$(uci get uhttpd.main.home)"/"
-echo "<root>"  > $path"xml/devices.xml"
+path=$(uci get uhttpd.main.home)
+jsonPath=$path"/json/devices.json"
+echo "["  > $jsonPath
 macAddresses=$(iw dev wlan0 station dump | awk '/Station/ {print $2}')
 for MAC in $macAddresses
 do
@@ -23,20 +24,12 @@ hw=$(echo $dhcp | awk '{print $1}')
 ip=$(echo $dhcp | awk '{print $2}')
 name=$(echo $dhcp | awk '{print $3}')
 dns=$(cat $path"dns/log" | grep query | grep [A] | grep $ip | tail -n 50 | awk '{print $6}')
-echo "<item>"  >> $path"xml/devices.xml"
-echo "<name>"  >> $path"xml/devices.xml"
-echo "$name"  >> $path"xml/devices.xml"
-echo "</name>"  >> $path"xml/devices.xml"
-echo "<macAddress>"  >> $path"xml/devices.xml"
-echo $hw >> $path"xml/devices.xml"
-echo "</macAddress>" >> $path"xml/devices.xml"
-echo "<ipAddress>" >> $path"xml/devices.xml"
-echo $ip >> $path"xml/devices.xml"
-echo "</ipAddress>" >> $path"xml/devices.xml"
-echo "<dns>" >> $path"xml/devices.xml"
-echo $dns >> $path"xml/devices.xml"
-echo "</dns>" >> $path"xml/devices.xml"
-echo "</item>" >> $path"xml/devices.xml"
+echo "{"  >> $jsonPath
+echo "\"name\" : \""$name"\","  >> $jsonPath
+echo "\"macAddress\" : \""$hw"\","  >> $jsonPath
+echo "\"ipAddress\" : \""$ip"\"," >> $jsonPath
+echo "\"dns\" : \""$dns"\"" >> $jsonPath
+echo "}," >> $jsonPath
 done
-echo "</root>" >> $path"xml/devices.xml"
+echo "]" >> $jsonPath
 echo "Success"
